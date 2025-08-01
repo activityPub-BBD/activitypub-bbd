98d8d7df8dd0ff55d00c868bb1abdf6c75898adb
SameerGhela <sameer.ghela@bbd.co.za>
2025-08-01 14:34:52 +0200

    notifications tab frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+/*import React from 'react';
 import { AppRoutes } from './routes/AppRoutes';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <AppRoutes />
+    </div>
+  );
+};
+
+export default App; */
+
+import React from 'react';
+import Notifications from './pages/Notifications';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Notifications />
     </div>
   );
 };
