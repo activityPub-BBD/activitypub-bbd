4be12e91bb045e32cc4e9cafba9998730e2811ad
Nomcebo-Ndimande <sibongile@bbd.co.za>
2025-08-02 21:34:16 +0200

    clear local storage on logout

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -28,6 +28,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const logout = () => {
     localStorage.removeItem('jwt');
     localStorage.removeItem('user');
+    localStorage.clear();
     setJwt(null);
     setUser(null);
   };
