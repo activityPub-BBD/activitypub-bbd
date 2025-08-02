c741a0bfc8764c8e7a0a33a67bf2cbce13599866
SameerGhela <sameer.ghela@bbd.co.za>
2025-08-02 20:00:46 +0200

    Refactor App component to remove commented-out code and unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-/*import React from 'react';
+import React from 'react';
 import { AppRoutes } from './routes/AppRoutes';
 
 const App: React.FC = () => {
@@ -9,18 +9,5 @@ const App: React.FC = () => {
   );
 };
 
-export default App; */
+export default App; 
 
-import React from 'react';
-import CreatePost from './pages/CreatePost';
-
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <CreatePost />
-    </div>
-  );
-};
-
-export default App;
