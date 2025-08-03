f19c9a34b4cb714c526fbd695850bb160a8c5512
Cindi-Ann Jeffery <cindi-anne.jeffery@bbd.co.za>
2025-08-03 17:24:43 +0200

    removed query client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,16 +4,12 @@ import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import './styles/global.css';
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from './utils/queryClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <AuthProvider>
       <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
           <App />
-        </QueryClientProvider>
       </BrowserRouter>
     </AuthProvider>
   </StrictMode>,
