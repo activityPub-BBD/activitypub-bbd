6a246eb4b069c15b22071c5e9e5429758be1ec3e
Mukhunyeledzi-Muthuphei <Mukhunyeledzi.Muthuphei@bbd.co.za>
2025-08-04 11:21:52 +0200

    adding s3 role to the ec2

--- terraform/infrastructure/ec2.tf
+++ terraform/infrastructure/ec2.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "group_5_mastodon_ec2_instance" {
   key_name                    = "group-5-mastodon-key"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_s3_profile.name
 
 
   user_data = <<-EOF
@@ -45,5 +46,55 @@ resource "aws_eip" "group_5_mastodon_ec2_eip" {
 output "ec2_ip" {
   value       = aws_eip.group_5_mastodon_ec2_eip.public_ip
   description = "Public IP of EC2 API server"
+}
+
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "group-5-mastodon-ec2-s3-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "group_5_mastodon_ec2_s3_role"
+  }
+}
+
+resource "aws_iam_policy" "ec2_s3_policy" {
+  name        = "group-5-mastodon-ec2-s3-policy"
+  description = "Policy for EC2 to perform CRUD operations on S3 media bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "s3:*"
+        Resource = [
+          aws_s3_bucket.media_bucket.arn,
+          "${aws_s3_bucket.media_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_s3_policy_attachment" {
+  role       = aws_iam_role.ec2_s3_role.name
+  policy_arn = aws_iam_policy.ec2_s3_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_s3_profile" {
+  name = "group-5-mastodon-ec2-s3-profile"
+  role = aws_iam_role.ec2_s3_role.name
 }
 
