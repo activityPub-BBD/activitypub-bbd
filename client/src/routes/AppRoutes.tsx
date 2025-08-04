aac16ff1f92e7f209f8f0b55d9d4ac4577d03b92
Nomcebo-Ndimande <sibongile@bbd.co.za>
2025-08-04 19:26:24 +0200

    added user profile page - for when you search a user and click on them, displays their profile

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -9,6 +9,8 @@ import UserSearch from '../pages/UserSearch';
 import { FollowersList } from '../pages/FollowersTab';
 import { FollowingList } from '../pages/FollowingTab';
 import Notifications from '../pages/Notifications';
+import UserProfilePage from '../pages/UserProfilePage';
+
 
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const { user } = useAuthContext();
@@ -74,6 +76,14 @@ export const AppRoutes = () => {
         element={
           <PrivateRoute>
             <UserSearch />
+          </PrivateRoute>
+        } 
+      />
+      <Route 
+        path="/users/:username" 
+        element={
+          <PrivateRoute>
+            <UserProfilePage />
           </PrivateRoute>
         } 
       />
