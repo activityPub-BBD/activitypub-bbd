c0bc604a342dc4906fa6ad19907e0157f2dd4428
Mukhunyeledzi-Muthuphei <Mukhunyeledzi.Muthuphei@bbd.co.za>
2025-08-06 19:41:21 +0200

    fixing deployment error

--- terraform/infrastructure/api_gateway.tf
+++ terraform/infrastructure/api_gateway.tf
@@ -75,5 +75,5 @@ resource "aws_api_gateway_domain_name" "mastodon_domain" {
 resource "aws_api_gateway_base_path_mapping" "mastodon_mapping" {  
   api_id      = aws_api_gateway_rest_api.mastodon_api.id
   stage_name  = aws_api_gateway_stage.mastodon_stage.stage_name
-  domain_name = aws_api_gateway_domain_name.mastodon_domain[0].domain_name
+  domain_name = aws_api_gateway_domain_name.mastodon_domain.domain_name
 }
